30abbc6d95d6df37a79d3cea20860a78c8a4d7af
nitinsampath <nitinsampath@airbears2-10-142-78-179.airbears2.1918.berkeley.edu>
2017-01-17 15:06:17 -0800

    Initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,24 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=7086b3fb7be9dac0c2ea4d6490a2c4cb&units=imperial';
+
+
+
+
+module.exports = {
+	getTemp: function(location) {
+		var encodedLocation = encodeURIComponent(location);
+		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+	
+		return axios.get(requestUrl).then(function(res){
+			debugger;
+			if (res.data.cod && res.data.message) {
+				throw new Error(res.data.message);
+			} else {
+				return res.data.main.temp;
+			}
+		}, function (res) {
+			throw new Error(res.data.message);
+		});
+	}
+}
---
+++ example-arrow-function.js
@@ -0,0 +1,15 @@
+function add (a, b){
+	return a + b;
+}
+
+
+
+var addStatement = (a, b) => {
+	return a + b; 
+};
+
+console.log(addStatement(2, 3));
+
+var addExpression = (a, b) => a + b;
+
+console.log(addExpression(2, 3));
