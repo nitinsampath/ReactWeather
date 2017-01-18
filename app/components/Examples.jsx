1755226674ced393b476f9353112fc86c641913b
nitinsampath <nitinsampath@calvisitor-10-105-131-23.calvisitor.1918.berkeley.edu>
2017-01-17 20:12:50 -0800

    rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -5,8 +5,8 @@ var React = require('react');
 var Examples = (props) => {
 	return(
 			<div>
-				<h3>Examples Component!</h3>
-				<p> Welcome </p>
+				<h3>Examples</h3>
+				<p> Welcome to the example page!</p>
 			</div>
 		);
 };
